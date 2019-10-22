062262e37dc83a6ac56d71dfddbb06be8b43ab8c
Roman Kolesnev <roman.kolesnev@vineti.com>
2019-10-22 12:57:52 +0400

    feat: keybindings for Ruby mode

--- modules/lang-ruby.el
+++ modules/lang-ruby.el
@@ -7,22 +7,52 @@
   :interpreter "ruby"
   :init
   (setq enh-ruby-add-encoding-comment-on-save nil)
-  (setq enh-ruby-deep-indent-paren nil))
+  (setq enh-ruby-deep-indent-paren nil)
+  :general
+  (my/leader
+    :keymaps 'enh-ruby-mode-map
+    "m" '(:prefix-command spc-ruby-map :wk "Ruby")))
 
 (use-package rspec-mode
-  :after enh-ruby-mode)
+  :after enh-ruby-mode
+  :general
+  (spc-ruby-map
+   "t" '(:prefix-command spc-ruby-rspec-map :wk "RSpec"))
+  (spc-ruby-rspec-map
+   "b" #'rspec-verify-matching
+   "a" #'rspec-verify-all
+   "c" #'rspec-verify-continue
+   "t" #'rspec-verify-single))
 
 (use-package yard-mode
   :after enh-ruby-mode
   :hook enh-ruby-mode)
 
 (use-package rubocop
-  :after enh-ruby-mode)
+  :after enh-ruby-mode
+  :general
+  (spc-ruby-map
+   "=" #'rubocop-autocorrect-current-file
+   "r" '(:prefix-command spc-ruby-rubocop-map :wk "Rubocop"))
+  (spc-ruby-rubocop-map
+   "a" #'rubocop-check-project
+   "A" #'rubocop-autocorrect-project
+   "d" #'rubocop-check-directory
+   "D" #'rubocop-autocorrect-directory
+   "f" #'rubocop-check-current-file
+   "F" #'rubocop-autocorrect-current-file))
 
 (use-package rbenv
   :after enh-ruby-mode
   :config
-  (global-rbenv-mode))
+  (global-rbenv-mode)
+  :general
+  (spc-ruby-map
+   "R" '(:prefix-command spc-ruby-rbenv-map))
+  (spc-ruby-rbenv-map
+   "u" #'rbenv-use
+   "g" #'rbenv-use-global
+   "c" #'rbenv-use-corresponding))
 
 (provide 'lang-ruby)
 ;;; lang-ruby.el ends here
